b12ae87c98b5e274e4d1516adc5c4f83f3636923
anton <antonzorikov@mail.ru>
2023-01-22 00:12:04 +0300

    added resize, emplace and inser function

--- Linked list.cpp
+++ Linked list.cpp
@@ -9,6 +9,7 @@ struct Node
 	T data;
 	Node();
 	Node(T data_c) : data(data_c){};
+	Node(T data_c, Node* next_node_c) : data(data_c), next_node(next_node_c){};
 };
 
 template<typename T>
@@ -64,15 +65,81 @@ struct List
 			return false;
 		}
 	}
+	void resize(int new_size) {
+		if (new_size > size) {
+			while (new_size > size) {
+				last->next_node = new Node<T>(NULL);
+				last = last->next_node;
+				size++;
+			}
+		}
+		else if(new_size < size)
+		{
+			while (new_size < size)
+			{
+				Node<T>* node = first;
+				while (node->next_node != last) {
+					node = node->next_node;
+				}
+				node->next_node = nullptr;
+				last = node;
+				size--;
+			}
+		}
+	}
+	void emplace(int position, T val) {
+		int s = 0;
+		Node<T>* node = first;
+		if (size < position || position < 0) {
+			cout << "Error: Insertion position is not correct" << endl;
+		}
+		else {
+			Node<T>* node = first;
+			while (s != position - 1) {
+				node = node->next_node;
+				s++;
+				if (s == position - 1) {
+
+					node->next_node = new Node<T>(val, (node->next_node)->next_node);
+					break;
+				}
+			}
+		}
+	}
+	void insert(int position, T val) {
+		int s = 0;
+		Node<T>* node = first;
+		if (size < position || position < 0) {
+			cout << "Error: Insertion position is not correct" << endl;
+		}
+		else {
+			Node<T>* node = first;
+			while (s != position - 1) {
+				node = node->next_node;
+				s++;
+				if (s == position - 1) {
+
+					node->next_node = new Node<T>(val, node->next_node);
+					break;
+				}
+			}
+		}
+		size++;
+	}
 };
 
 int main()
 {
 	List<int> list;
-	cout << list.empty();
 	list.push_back(1);
-	cout << list.empty();
-	list.pop_back();
-	cout << list.empty();
+	list.push_back(2);
+	list.push_back(3);
+	list.push_back(4);
+	list.push_back(5);
+	list.push_back(6);
+	list.push_back(7);
+	list.insert(1, 50);
+	list.show_list();
+	//emplace and insert cant work with 1 and 2
 }
 
